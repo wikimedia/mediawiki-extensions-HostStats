71bbce8ddbea71933388c4149e7e001a97bb4688
Hydriz <admin@alphacorp.tk>
2012-11-04 10:03:12 +0800

    Adding message documentation.
    Per Raymond's suggestion in change I0188900f, this change will add
    message documentation strings so that it can be translated on
    Translatewiki.net.
    
    Note: I am open to suggestions to change the usage of the phrase
    "Host statistics", as it can be quite confusing. It generally means
    the information about the server.
    
    Change-Id: I67fcda8a9048927ed0312eaed2e263a38c532265

--- HostStats.i18n.php
+++ HostStats.i18n.php
@@ -17,3 +17,12 @@ $messages['en'] = array(
 	'hoststats-desc' => 'A tool to output the host statistics that the wiki is on.',
 	'hoststats-intro' => 'This page shows some host statistics that the wiki is on.',
 );
+
+/** Message documentation (Message documentation)
+ * @author Hydriz
+ */
+$messages['qqq'] = array(
+	'hoststats-title' => 'The title of the special page. "Host" refers to the server the wiki is on.',
+	'hoststats-desc' => '{{desc}}',
+	'hoststats-intro' => 'The introduction to the special page. "Host" refers to the server the wiki is on.',
+);
