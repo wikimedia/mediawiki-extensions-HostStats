8b75ab381ea2793825dcd154f2464f7f5aecb7f0
raymond <raimond.spekking@gmail.com>
2012-11-20 22:30:54 +0100

    Remove period from end of description message
    for consistency, spotted by Siebrand
    https://translatewiki.net/wiki/Thread:Support/About_MediaWiki:Hoststats-desc/en
    
    Change-Id: Ibc1c506d7dbaefa6b435f7387b5172cdfcc5657a

--- HostStats.i18n.php
+++ HostStats.i18n.php
@@ -14,7 +14,7 @@ $messages = array();
 $messages['en'] = array(
 	'hoststats' => '{{int:hoststats-title}}', // Do not translate this message
 	'hoststats-title' => 'Host statistics',
-	'hoststats-desc' => 'A tool to output the host statistics that the wiki is on.',
+	'hoststats-desc' => 'A tool to output the host statistics that the wiki is on',
 	'hoststats-intro' => 'This page shows some host statistics that the wiki is on.',
 );
 
