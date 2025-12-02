2e45bf3f0e3c0d936ee5550ffe33f9e07b5554c0
anterdc99 <xuminruixuan@live.cn>
2025-12-02 19:11:12 +0800

    HostStats: Set Chinese primary special page alias to the English primary
    Bug: T399285
    Change-Id: Icf9e64d84c60eddc9d3029384a246262a1c1f6bb

--- HostStats.alias.php
+++ HostStats.alias.php
@@ -58,12 +58,17 @@ $specialPageAliases['sv'] = [
 	'HostStats' => [ 'Värdstatistik' ],
 ];
 
-/** Simplified Chinese (中文（简体）‎) */
+/** Chinese (中文) */
+$specialPageAliases['zh'] = [
+	'HostStats' => [ 'HostStats' ],
+];
+
+/** Simplified Chinese (中文（简体）) */
 $specialPageAliases['zh-hans'] = [
 	'HostStats' => [ '主机统计' ],
 ];
 
-/** Traditional Chinese (中文（繁體）‎) */
+/** Traditional Chinese (中文（繁體）) */
 $specialPageAliases['zh-hant'] = [
 	'HostStats' => [ '主機統計' ],
 ];
